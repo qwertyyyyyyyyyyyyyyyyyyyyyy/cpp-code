fec3c1e87ae0916c90cdb290ce1f8842bf0ec908
witqwerty1234 <zhengwit2@gmail.com>
2023-03-30 16:55:21 +0300

    macro of cpp

--- unit04/demo01/main.cpp
+++ unit04/demo01/main.cpp
@@ -6,6 +6,9 @@ int main(void)
 	int size = 5;
 	std::cout<<"Value of SIZE(macro)"<<SIZE<<std::endl;
 	std::cout<<"Value of size(variable)"<<size<<std::endl;
+	const long long id = 123456789;
+	std::cout<<"Valueof id is "<<id<<std::endl;
+	
 	
 	return 0;
 }
