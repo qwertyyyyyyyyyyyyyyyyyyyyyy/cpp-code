23e85a5299208032f9737b598a0f60692e0c077b
witqwerty1234 <zhengwit2@gmail.com>
2023-03-30 16:47:47 +0300

    macro of cpp 01

---
+++ unit04/demo01/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define SIZE 5
+
+int main(void)
+{
+	int size = 5;
+	std::cout<<"Value of SIZE(macro)"<<SIZE<<std::endl;
+	std::cout<<"Value of size(variable)"<<size<<std::endl;
+	
+	return 0;
+}
